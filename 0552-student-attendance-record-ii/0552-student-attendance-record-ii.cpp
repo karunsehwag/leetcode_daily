90e19461e724e016b749dbf341bb72b6d1e761cd
KARUN CHOUDHARY <72781081+karunsehwag@users.noreply.github.com>
2024-05-26 23:23:57 +0530

    Time: 1506 ms (8.84%), Space: 509.4 MB (5.12%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -1,41 +1,26 @@
 class Solution {
     int MOD = 1000000007;
-    // Cache to store sub-problem results.
-    vector<vector<vector<int>>> memo;
-
 public:
-    // Recursive function to return the count of combinations of length 'n' eligible for the award.
-    int eligibleCombinations(int n, int totalAbsences, int consecutiveLates) {
-        // If the combination has become not eligible for the award,
-        // then we will not count any combinations that can be made using it.
-        if (totalAbsences >= 2 or consecutiveLates >= 3) {
-            return 0;
-        }
-        // If we have generated a combination of length 'n' we will count it.
-        if (n == 0) {
-            return 1;
-        }
-        // If we have already seen this sub-problem earlier, we return the stored result.
-        if (memo[n][totalAbsences][consecutiveLates] != -1) {
-            return memo[n][totalAbsences][consecutiveLates];
-        }
-
-        int count = 0;
-        // We choose 'P' for the current position.
-        count = eligibleCombinations(n - 1, totalAbsences, 0);
-        // We choose 'A' for the current position.
-        count = (count + eligibleCombinations(n - 1, totalAbsences + 1, 0)) % MOD;
-        // We choose 'L' for the current position.
-        count = (count + eligibleCombinations(n - 1, totalAbsences, consecutiveLates + 1)) % MOD;
-
-        // Return and store the current sub-problem result in the cache.
-        return memo[n][totalAbsences][consecutiveLates] = count;
+    int backtrack(int n,int ab,int l,vector<vector<vector<int>>>& vec)
+    {
+      if(ab>=2||l>=3)
+          return 0;
+      if(n==0)
+          return 1;
+        
+        if(vec[n][ab][l]!=-1)
+            return vec[n][ab][l];
+int count=0;       
+count=backtrack(n-1,ab,0,vec);
+count=(count+backtrack(n-1,ab+1,0,vec))%MOD;
+count=(count+backtrack(n-1,ab,l+1,vec))%MOD;        
+        
+        return vec[n][ab][l]=count;
     }
-
     int checkRecord(int n) {
-        // Initialize the cache.
-        memo = vector<vector<vector<int>>>(n + 1, vector<vector<int>>(2, vector<int>(3, -1)));
-        // Return count of combinations of length 'n' eligible for the award.
-        return eligibleCombinations(n, 0, 0);
+        string str="";
+        vector<vector<vector<int>>> vec(n+1,vector<vector<int>>(3,vector<int>(3,-1)));
+       return  backtrack(n,0,0,vec);
+        
     }
 };
