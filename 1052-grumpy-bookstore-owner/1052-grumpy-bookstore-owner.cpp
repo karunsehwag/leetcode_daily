57c7f27da14f29cff4ba0bdfba05e1181f84efe5
KARUN CHOUDHARY <72781081+karunsehwag@users.noreply.github.com>
2024-06-22 09:22:41 +0530

    Time: 56 ms (11.08%), Space: 35.8 MB (5.12%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -1,30 +1,39 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
-        int n = customers.size();
-        int unrealizedCustomers = 0;
-
-        for (int i = 0; i < minutes; i++) {
-            unrealizedCustomers += customers[i] * grumpy[i];
+        vector<int> vec;
+        int ans=0;
+        for(int i=0;i<grumpy.size();i++)
+        {
+            if(grumpy[i]==0)
+            {
+                ans+=customers[i];
+                vec.push_back(0);
+            }
+            else
+            {
+                vec.push_back(customers[i]);
+            }
         }
-
-        int maxUnrealizedCustomers = unrealizedCustomers;
-
-        for (int i = minutes; i < n; i++) {
+        int left=0;
+        int val=0,check=0;
+        for(int i=0;i<grumpy.size();i++)
+        {
+          if(minutes>i)
+              val+=vec[i];
+           else
+           {
+               val=val-vec[left];
+               left++;
+               val+=vec[i];
+           }
+            cout<<val<<" ";
+            check=max(val,check);
+        }
+        ans=ans+check;
         
-            unrealizedCustomers += customers[i] * grumpy[i];
-            unrealizedCustomers -= customers[i - minutes] * grumpy[i - minutes];
-
-            maxUnrealizedCustomers =
-                max(maxUnrealizedCustomers, unrealizedCustomers);
-        }
-
-        int totalCustomers = maxUnrealizedCustomers;
-
-        for (int i = 0; i < n; i++) {
-            totalCustomers += customers[i] * (1 - grumpy[i]);
-        }
-
-        return totalCustomers;
+        
+        return ans;
+        
     }
 };
