b71c2d828f3f04f53de1d763f3de0f64df13faf6
KARUN CHOUDHARY <72781081+karunsehwag@users.noreply.github.com>
2024-07-03 18:08:41 +0530

    Time: 459 ms (10.48%), Space: 175 MB (7.97%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
@@ -1,106 +1,83 @@
-class Solution {
-    bool static comp(vector<int>& a, vector<int>& b) { return a[0] > b[0]; }
-    int findparent(int node, vector<int>& parent)
-    {
-        if(node == parent[node])
-        {
+#include <vector>
+using namespace std;
+
+class Disjointset {
+    vector<int> rank, parent;
+public:
+    Disjointset(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findupar(int node) {
+        if (node == parent[node])
             return node;
-        }
-        return parent[node] = findparent(parent[node], parent);
-    }
-    bool unionn(int u, int v, vector<int>& parent, vector<int>& rank)
-    {
-        u = findparent(u, parent);
-        v = findparent(v, parent);
-
-        if(u != v)
-        {
-            if(rank[u] < rank[v])
-            {
-                parent[u] = v;
-            }
-            else if(rank[u] > rank[v])
-            {
-                parent[v] = u;
-            }
-            else
-            {
-                parent[v] = u;
-                rank[u]++;
-            }
-            return true;
-        }
-        return false;
+        return parent[node] = findupar(parent[node]);
     }
 
+    bool unionr(int u, int v) {
+        int p1 = findupar(u);
+        int p2 = findupar(v);
+        if (p1 == p2)
+            return false;
+        if (rank[p1] > rank[p2])
+            parent[p2] = p1;
+        else if (rank[p1] < rank[p2])
+            parent[p1] = p2;
+        else {
+            parent[p2] = p1;
+            rank[p1]++;
+        }
+        return true;
+    }
+};
+
+class Solution {
 public:
     int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
-        vector<int>parentAlice(n+1);
-        vector<int>parentBob(n+1);
-        vector<int>aliceRank(n+1);
-        vector<int>bobRank(n+1);
+        Disjointset r1(n), r2(n);
+        int remove = 0;
+        int mer1 = 0, mer2 = 0;
 
-        sort(edges.begin(),edges.end(),comp);
-
-        for(int i=0;i<parentAlice.size();i++)
-        {
-            parentAlice[i] = i;
-            parentBob[i] = i;
-            aliceRank[i] = 1;
-            bobRank[i] = 1;
-        }
-        int removeEdges = 0;
-        int mergeAlice = 1;
-        int mergeBob = 1;
-        for(auto it : edges)
-        {
-            if(it[0] == 3)
-            {
-                bool tempAlice = unionn(it[1],it[2],parentAlice,aliceRank);
-                bool tempBob = unionn(it[1],it[2],parentBob,bobRank);
-
-                if(tempAlice == true)
-                {
-                    mergeAlice++;
+        for (vector<int> it : edges) {
+            if (it[0] == 3) {
+                bool val1 = r1.unionr(it[1], it[2]);
+                bool val2 = r2.unionr(it[1], it[2]);
+                if (val1) {
+                    mer1++;
                 }
-                if(tempBob == true)
-                {
-                    mergeBob++;
+                if (val2) {
+                    mer2++;
                 }
-                if(tempAlice == false && tempBob == false)
-                {
-                    removeEdges++;
-                }
-            }
-            else if(it[0] == 1)
-            {
-                bool tempAlice = unionn(it[1],it[2],parentAlice,aliceRank);
-                if(tempAlice == true)
-                {
-                    mergeAlice++;
-                }
-                else
-                {
-                    removeEdges++;
-                }
-            }
-            else
-            {
-                bool tempBob = unionn(it[1],it[2],parentBob,bobRank);
-                if(tempBob == true)
-                {
-                    mergeBob++;
-                }
-                else
-                {
-                    removeEdges++;
+            
+                if (!val1 && !val2) {
+                    remove++;
                 }
             }
         }
-        if(mergeAlice != n || mergeBob != n)
-        {
+
+        for (vector<int> it : edges) {
+            if (it[0] == 1) {  
+                if (r1.unionr(it[1], it[2])) {
+                    mer1++;
+                } else {
+                    remove++;
+                }
+            } else if (it[0] == 2) { 
+                if (r2.unionr(it[1], it[2])) {
+                    mer2++;
+                } else {
+                    remove++;
+                }
+            }
+        }
+
+        if (mer1 != n - 1 || mer2 != n - 1) {
             return -1;
         }
-        return removeEdges;
+        return remove;
     }
 };
