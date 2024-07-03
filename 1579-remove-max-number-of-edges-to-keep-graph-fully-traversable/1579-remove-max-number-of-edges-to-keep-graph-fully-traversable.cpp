3de87f2d8f9ae5ca2c2f96d85dabb238f3a1e79c
KARUN CHOUDHARY <72781081+karunsehwag@users.noreply.github.com>
2024-07-03 18:09:52 +0530

    Time: 453 ms (11.90%), Space: 175 MB (9.16%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
@@ -1,83 +1,94 @@
-#include <vector>
-using namespace std;
-
-class Disjointset {
+class Disjointset
+{
     vector<int> rank, parent;
-public:
-    Disjointset(int n) {
-        rank.resize(n + 1, 0);
-        parent.resize(n + 1);
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
+    public:
+    Disjointset(int n)
+    {
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        for(int i=0;i<n+1;i++)
+        {
+            parent[i]=i;
         }
     }
-
-    int findupar(int node) {
-        if (node == parent[node])
+    
+    int findupar(int node)
+    {
+        if(node==parent[node])
             return node;
-        return parent[node] = findupar(parent[node]);
+        
+        return parent[node]=findupar(parent[node]);
     }
-
-    bool unionr(int u, int v) {
-        int p1 = findupar(u);
-        int p2 = findupar(v);
-        if (p1 == p2)
-            return false;
-        if (rank[p1] > rank[p2])
-            parent[p2] = p1;
-        else if (rank[p1] < rank[p2])
-            parent[p1] = p2;
-        else {
-            parent[p2] = p1;
+    
+    bool unionr(int u,int v)
+    {
+        int p1=findupar(u);
+        int p2=findupar(v);
+        if(p1==p2)
+            return 0;
+        if(rank[p1]>rank[p2])
+            parent[p2]=p1;
+        else if(rank[p2]>rank[p1])
+            parent[p1]=p2;
+        else
+        {
+            parent[p2]=p1;
             rank[p1]++;
         }
-        return true;
+        return 1;
     }
 };
-
 class Solution {
 public:
-    int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
-        Disjointset r1(n), r2(n);
-        int remove = 0;
-        int mer1 = 0, mer2 = 0;
-
-        for (vector<int> it : edges) {
-            if (it[0] == 3) {
-                bool val1 = r1.unionr(it[1], it[2]);
-                bool val2 = r2.unionr(it[1], it[2]);
-                if (val1) {
-                    mer1++;
+    int maxNumEdgesToRemove(int n, vector<vector<int>>& edges)
+    {
+        Disjointset r1(n),r2(n);
+        int remove=0;
+        int mer1=0,mer2=0;
+        for(vector<int> it:edges)
+        {
+            if(it[0]==3)
+            {   bool val1=r1.unionr(it[1],it[2]);
+                bool val2=r2.unionr(it[1],it[2]);
+                if(val1)
+                {
+                  mer1++;
                 }
-                if (val2) {
+                if(val2)
                     mer2++;
-                }
-            
-                if (!val1 && !val2) {
-                    remove++;
-                }
+                if(!val1&&!val2)
+                    remove++;        
             }
         }
-
-        for (vector<int> it : edges) {
-            if (it[0] == 1) {  
-                if (r1.unionr(it[1], it[2])) {
-                    mer1++;
-                } else {
-                    remove++;
+        for(vector<int> it:edges)
+        {
+            if(it[0]==1)
+            {
+               if(r1.unionr(it[1],it[2]))
+                {
+                  mer1++;
+                 
                 }
-            } else if (it[0] == 2) { 
-                if (r2.unionr(it[1], it[2])) {
-                    mer2++;
-                } else {
-                    remove++;
+                else
+                {
+                  remove++;  
+                }   
+            } 
+            else if(it[0]==2)
+            {
+               if(r2.unionr(it[1],it[2]))
+                {
+                  mer2++;
+                 
                 }
+                else
+                {
+                  remove++;  
+                }   
             }
         }
-
-        if (mer1 != n - 1 || mer2 != n - 1) {
+        if(mer1!=n-1 || mer2!=n-1)
             return -1;
-        }
-        return remove;
+        return remove;    
     }
 };
