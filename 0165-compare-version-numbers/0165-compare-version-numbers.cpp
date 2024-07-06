d9d829e20fafab2e29d384055a722754578496e9
KARUN CHOUDHARY <72781081+karunsehwag@users.noreply.github.com>
2024-07-06 10:05:03 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (78.64%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int compareVersion(string v1, string v2) {
+        int i = 0, j = 0;
+        while (i < v1.size() || j < v2.size()) {
+            int num1 = 0, num2 = 0;
+            while (i < v1.size() && v1[i] != '.') {
+                num1 = num1 * 10 + (v1[i] - '0');
+                i++;
+            }
+            while (j < v2.size() && v2[j] != '.') {
+                num2 = num2 * 10 + (v2[j] - '0');
+                j++;
+            }
+            if (num1 < num2) return -1;
+            if (num1 > num2) return 1;
+            i++;
+            j++;
+        }
+        return 0;
+    }
+};
