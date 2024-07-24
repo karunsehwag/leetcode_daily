1b07cd5ab008b9f3f042c60b3fd168222ec3949b
KARUN CHOUDHARY <72781081+karunsehwag@users.noreply.github.com>
2024-07-24 18:24:50 +0530

    Time: 635 ms (19.91%), Space: 377.2 MB (6.49%) - LeetHub

---
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+        
+        vector<vector<int>> vec(nums.size(),vector<int>(2,0));
+        for(int i=0;i<nums.size();i++)
+        {   int count=0;
+            int val=nums[i];
+            if(val==0)
+                vec[i][0]=mapping[val];
+         else
+         { 
+            stack<int> st;      
+            while(val>0)
+            {   
+                st.push(val%10);
+                val=val/10;
+            }
+           val=0;
+            while(!st.empty())
+            {
+                val=mapping[st.top()%10]+val*10;
+                st.pop();
+            }
+            cout<<val<<" ";
+            vec[i][0]=val;
+             
+         }
+            vec[i][1]=i;
+        }
+        vector<int> ans;
+        sort(vec.begin(),vec.end());
+        for(int i=0;i<nums.size();i++)
+        {
+            ans.push_back(nums[vec[i][1]]);
+        }
+        return ans;
+        
+    }
+};
