63a275b9d266a68aa573683cc325cef1f54f35a2
KARUN CHOUDHARY <72781081+karunsehwag@users.noreply.github.com>
2024-08-04 16:42:30 +0530

    Time: 8 ms (49.81%), Space: 18.6 MB (27.93%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -12,36 +12,32 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if (root == NULL)
+        
+        if(root==NULL)
             return 0;
-
         using ull = unsigned long long; // alias for unsigned long long
         std::queue<std::pair<TreeNode*, ull>> q;
-        int ans = 0;
-        q.push({root, 0});
-
-        while (!q.empty()) {
-            int size = q.size();
-            ull minv = q.front().second;
-            ull start = 0, last = 0;
-
-            for (int i = 0; i < size; ++i) {
-                ull val = q.front().second - minv;
-                TreeNode* node = q.front().first;
+        int ans=0;
+        q.push({root,0});
+        while(!q.empty())
+        {
+            ull minv=q.front().second;
+            ull size=q.size();
+            ull start=0,last=0;
+            for(int i=0;i<size;i++)
+            {
+                ull val=q.front().second-minv;
+                TreeNode* tree=q.front().first;
                 q.pop();
-
-                if (i == 0) start = val;
-                if (i == size - 1) last = val;
-
-                if (node->left)
-                    q.push({node->left, val * 2});
-                if (node->right)
-                    q.push({node->right, val * 2 + 1});
+                if(i==0) start=val;
+                if(i==size-1) last=val;
+                if(tree->left)
+                    q.push({tree->left,val*2});
+                if(tree->right)
+                    q.push({tree->right,val*2+1});
             }
-
-            ans = std::max(ans, int(last + 1 - start));
+            ans=max(ans,int(last+1-start));
         }
-
         return ans;
     }
 };
