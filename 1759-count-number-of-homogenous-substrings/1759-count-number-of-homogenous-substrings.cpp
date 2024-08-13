345558b05c009e05b93c9205910c74682806cac7
KARUN CHOUDHARY <72781081+karunsehwag@users.noreply.github.com>
2024-08-14 01:46:46 +0530

    Time: 26 ms (65.73%), Space: 13.6 MB (42.57%) - LeetHub

---
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countHomogenous(string s) {
+        int ans=0;
+        int curr=0;
+        int MOD=1e9+7;
+        for(int i=0;i<s.size();i++)
+        {
+            if(i==0||s[i]==s[i-1])
+                curr++;
+             else
+                 curr=1;
+        
+        
+        ans=(ans+curr)%MOD;
+        }
+        return ans;
+        
+    }
+};
