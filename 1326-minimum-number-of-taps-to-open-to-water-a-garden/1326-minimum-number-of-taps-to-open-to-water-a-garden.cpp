96e9597f2b18ca817190f1b7b99ec32a9d4fffc3
KARUN CHOUDHARY <72781081+karunsehwag@users.noreply.github.com>
2024-08-15 00:41:37 +0530

    Time: 31 ms (14.50%), Space: 22.7 MB (12.15%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,10 +1,5 @@
 class Solution {
 public:
-    static bool comp(const std::vector<int>& a, const std::vector<int>& b) {
-    if (a[0] == b[0])
-        return a[1] > b[1];
-    return a[0] < b[0];
-}
     int minTaps(int n, vector<int>& ranges) {
         
         vector<vector<int>> vec(ranges.size(),vector<int>(2,0));
@@ -15,13 +10,12 @@ public:
             vec[i][1]=i+ranges[i]; 
         }
         }
-        sort(vec.begin(),vec.end(),comp);
+        sort(vec.begin(),vec.end());
         
         int count = 0;
         int currentEnd = 0;
         int farthest = 0;
         int i = 0;
-        
          while (currentEnd < n) {
             while (i < vec.size() && vec[i][0] <= currentEnd) {
                 farthest = max(farthest, vec[i][1]);
