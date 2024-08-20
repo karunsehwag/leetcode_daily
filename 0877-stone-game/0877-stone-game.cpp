275bb4a1c97ad2a7fe9e9bac0b0c6554dcfc0a97
KARUN CHOUDHARY <72781081+karunsehwag@users.noreply.github.com>
2024-08-21 01:22:06 +0530

    Time: 19 ms (16.18%), Space: 18.6 MB (11.33%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,7 +1,25 @@
 class Solution {
 public:
+    int rec(vector<int>& piles,int i,int n,vector<vector<int>>& vec)
+    {
+        if(i>n)
+            return 0;
+        if(vec[i][n]!=-1)
+            return vec[i][n];
+        int s1=piles[i]+rec(piles,i+1,n,vec);
+        int s2=piles[n]+rec(piles,i,n-1,vec);
+        return vec[i][n]=max(s1,s2);
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        long long sum=0;
+        
+        for(int x:piles)
+            sum+=x;
+        vector<vector<int>> vec(piles.size()+1,vector<int>(piles.size()+1,-1));
+        int  ans=rec(piles,0,piles.size()-1,vec);
+        if(ans>sum/2)
+            return 1;
+        else return 0;
         
     }
 };
