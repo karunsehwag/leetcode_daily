b98de0366943fc3fd97160912cc25f360954f964
KARUN CHOUDHARY <72781081+karunsehwag@users.noreply.github.com>
2024-08-29 23:39:03 +0530

    Time: 31 ms (72.35%), Space: 30.4 MB (5.34%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,36 +1,40 @@
 class Solution {
 public:
     int removeStones(vector<vector<int>>& stones) {
-        unordered_map<int, vector<int>> mp;
-
-        // Create adjacency list for rows and columns
-        for (int i = 0; i < stones.size(); i++) {
-            mp[stones[i][0]].push_back(stones[i][1] + 10001); // Offset for column values
-            mp[stones[i][1] + 10001].push_back(stones[i][0]); // Reverse mapping for DFS
-        }
-
-        vector<int> vis(20002, 0); // Larger size to accommodate the offset
-
-        // DFS to count connected components
-        int components = 0;
-        for (int i = 0; i < stones.size(); i++) {
-            int x = stones[i][0];  // Row
-            if (!vis[x]) {
-                components++;
-                dfs(x, mp, vis);
-            }
-        }
-
-        return stones.size() - components; // Max moves = total stones - connected components
-    }
-
-    // Depth-first search
-    void dfs(int node, unordered_map<int, vector<int>>& mp, vector<int>& vis) {
-        vis[node] = 1;
-        for (int neighbor : mp[node]) {
-            if (!vis[neighbor]) {
-                dfs(neighbor, mp, vis);
-            }
-        }
+    unordered_map<int,vector<int>> mp;
+    for(int i=0;i<stones.size();i++)
+    {   
+       mp[stones[i][0]].push_back(stones[i][1]+10001);
+       mp[stones[i][1]+10001].push_back(stones[i][0]);
+    } 
+ 
+     stack<int> st;
+     vector<int> vis(20002,0);
+     int count=0;
+     for(int i=0;i<stones.size();i++)
+     {
+         if(vis[stones[i][0]]==0)
+         {   count++;
+             st.push(stones[i][0]);
+             while(!st.empty())
+             {
+                int node=st.top();
+                st.pop();
+                vis[node]=1;
+                for(int it:mp[node])
+                {   
+                    if(vis[it]==0)
+                     {   
+                        st.push(it);   
+                    }
+                }
+                 
+             }   
+             
+         }
+     }
+        
+        return stones.size()-count;
+        
     }
 };
