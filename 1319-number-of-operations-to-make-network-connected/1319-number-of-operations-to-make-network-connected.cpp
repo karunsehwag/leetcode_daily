16c84a467f986640b173cae5ed80830a75ca04f8
KARUN CHOUDHARY <72781081+karunsehwag@users.noreply.github.com>
2024-08-31 13:34:32 +0530

    Time: 131 ms (8.46%), Space: 65.9 MB (5.84%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,63 +1,35 @@
 class Solution {
 public:
-    class Disjointset{
-        
-        vector<int> parent,size;
-        public:
-        Disjointset(int n)
-        {
-            size.resize(n+1,1);
-            parent.resize(n+1);
-            for(int i=0;i<n;i++)
-                parent[i]=i;
-        }
-        
-        int findupar(int node)
-        {
-            if(node==parent[node])
-                return node;
-            return parent[node]=findupar(parent[node]);
-                
-        }
-        void unionsize(int u,int v)
-        {
-            int u_u=findupar(u);
-            int u_v=findupar(v);
-            if(u_u==u_v)
-                return;
-            if(size[u_u]>size[u_v])
+    void dfs(int n,unordered_map<int,vector<int>>& mp,vector<int>& vis,int& edges)
+    {
+        vis[n]=1;
+        for(int it:mp[n])
+        {  
+            if(vis[it]==0)
             {
-                parent[u_v]=u_u;
-                size[u_u]+=size[u_v];
-            }
-            else
-            {
-               parent[u_u]=u_v;
-               size[u_v]+=size[u_u];  
+                dfs(it,mp,vis,edges); 
             }
         }
-    };
-    
+    }
     int makeConnected(int n, vector<vector<int>>& connections) {
-        Disjointset Ds(n);
-        int counte=0;
+        if(connections.size()<n-1)
+            return -1;
+        int count=connections.size();
+        vector<int> vis(n+1,0);
+        unordered_map<int,vector<int>> mp;
+        int edges=0;
         for(vector<int> it:connections)
-        {   if(Ds.findupar(it[0])!=Ds.findupar(it[1]))
-                Ds.unionsize(it[0],it[1]);
-            else
-                counte++;
-        }
-        int count=0;
-        for(int i=0;i<n;i++)
         {
-            if(i==Ds.findupar(i))
-            {
-                count++;
-            }
+            mp[it[0]].push_back(it[1]);
+            mp[it[1]].push_back(it[0]);
         }
-        if(counte>=count-1)
-            return count-1;
-        
-        return -1;   
+        int island=0;
+        for(int i=0;i<n;i++)
+        {  if(vis[i]==0)
+              {   island++;
+                 dfs(i,mp,vis,edges);
+              }  
+        }
+            return island-1;
     }
 };
