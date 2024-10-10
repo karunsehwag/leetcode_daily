db31280900ba950cfb0e6e473c952a06d446f50a
KARUN CHOUDHARY <72781081+karunsehwag@users.noreply.github.com>
2024-10-10 13:31:31 +0530

    Time: 4 ms (70.79%), Space: 12.2 MB (60.34%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int> st;
         map<int,int> mp;
-        for( int i=nums2.size()-1;i>=0;i--)
+        stack<int> st;
+        for(int i=0;i<nums2.size();i++)
         {
-            if(st.empty())
+            while(!st.empty()&&nums2[st.top()]<nums2[i])
             {
-                st.push(nums2[i]);
-                mp[nums2[i]]=-1;
+                mp[nums2[st.top()]]=nums2[i];
+                st.pop();
             }
-            else
-            {
-                while(!st.empty()&&st.top()<nums2[i])
-                {
-                    st.pop();
-                    
-                }
-                if(st.empty())
-                {
-                   mp[nums2[i]]=-1; 
-                }
-                else
-                {
-                   mp[nums2[i]]=st.top(); 
-                }
-                st.push(nums2[i]);
-            }
-            
+            mp[nums2[i]]=-1;
+            st.push(i);            
         }
-        vector<int> ans;
-        for(int x:nums1)
-        {
-            ans.push_back(mp[x]);
-        }
+    vector<int> ans;
+    for(int x:nums1)
+    {
+        ans.push_back(mp[x]);
+    }
         return ans;
         
     }
