ca50eecf36b9d9e8d6b89a338e469383f2c0a447
KARUN CHOUDHARY <72781081+karunsehwag@users.noreply.github.com>
2024-10-15 01:46:05 +0530

    Time: 10 ms (49.54%), Space: 18.7 MB (59.92%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,44 +2,43 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int count1=0,count2=0;
-        int cand1=0,cand2=0;
+        int cand1=INT_MIN,cand2=INT_MIN;
         for(int x:nums)
         {
             if(count1==0&&x!=cand2)
             {
-              count1++;
-              cand1=x;
-            } 
-            else if(count2==0&&x!=cand1)
-            {
-              count2++;
-              cand2=x;
+                cand1=x;
+                count1=1;
             }
-            else if(cand1==x)
-            count1++;
+            else if(count2==0&&x!=cand1)
+            {    
+                cand2=x;
+                count2=1;
+            }
+           else if(cand1==x)
+                 count1++;
             else if(cand2==x)
-             count2++;
-             else
-             {
-               count1--;
-               count2--;
-             }
-
+                  count2++;
+            else 
+            {
+                count1--;
+                count2--;
+            }
         }
+        vector<int> vec;
         count1=0,count2=0;
         for(int x:nums)
         {
-          if(x==cand1)
-            count1++;
-            else if(x==cand2)
-               count2++;
+            if(cand1==x)
+                count1++;
+            if(cand2==x)
+                count2++;
         }
-        vector<int> vec;
-        int threshold=nums.size()/3;
-        if(count1>threshold)
-           vec.push_back(cand1);
-        if(count2>threshold)
-           vec.push_back(cand2);   
-      return vec;  
+        if(count1>floor(nums.size()/3))
+            vec.push_back(cand1);
+        if(count2>floor(nums.size()/3))
+            vec.push_back(cand2);
+        return vec;
+        
     }
 };
