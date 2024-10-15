4024d00e5e7b79418eb5e5308f79903fe36c2a05
KARUN CHOUDHARY <72781081+karunsehwag@users.noreply.github.com>
2024-10-16 01:52:36 +0530

    Time: 35 ms (42.12%), Space: 15.6 MB (76.03%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -4,15 +4,8 @@ public:
         
         int n=s.size()-1;
         long long ans=0;
-        for(int i=s.size()-1;i>=0;i--)
-        {
-            if(s[i]=='1')
-            {
-                ans+=n-i;
-                n--;
-            }
+        for(int i=s.size()-1;i>=0;i--)ans+=(s[i]=='1')?n-- -i:0;
             
-        }
         return ans;
         
     }
