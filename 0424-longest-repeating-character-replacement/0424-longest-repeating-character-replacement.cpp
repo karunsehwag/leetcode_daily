c5659fab8ad28e80bdc0fb4a24d59d73bcb5a88e
KARUN CHOUDHARY <72781081+karunsehwag@users.noreply.github.com>
2024-10-19 13:44:51 +0530

    Time: 7 ms (72.68%), Space: 9.5 MB (41.89%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int left=0,right=0;
-        int ans=0,maxi=0;
+        int left=0,right=0,maxi=0,ans=0;
         unordered_map<char,int> mp;
         while(s.size()>right)
         {
@@ -13,10 +12,9 @@ public:
                 mp[s[left]]--;
                 left++;
             }
-            ans=max(ans,static_cast<int>(right-left+1));
-            right++;
+           ans=max(ans,static_cast<int>(right-left+1)); 
+           right++;
         }
-        return ans;
-        
+        return ans;    
     }
 };
