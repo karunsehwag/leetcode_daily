c70470685da158ea1e864ca840014b847297669d
KARUN CHOUDHARY <72781081+karunsehwag@users.noreply.github.com>
2024-10-23 00:45:18 +0530

    Time: 41 ms (92.82%), Space: 47.9 MB (17.71%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        int freq[26]={0};
-        for(char & ch:tasks)
-        {
-            freq[ch-'A']++;
-        }
+        map<char,int> freq;
+        int ans=0;
+        for(char ch:tasks)
+            freq[ch]++;
         priority_queue<int> pq;
-        for(int i=0;i<26;i++)
-        {
-            if(freq[i]>0)
-                pq.push(freq[i]);
-        }
-        int time=0;
+        for(auto it:freq)
+            pq.push(it.second);
         while(!pq.empty())
-        {
+        {   
             int cycle=n+1;
             vector<int> store;
             int task=0;
             while(cycle--&&!pq.empty())
-            {
-                if(pq.top()>1)
-                    store.push_back(pq.top()-1);
-                pq.pop();
+            {   if(pq.top()>1)
+                  store.push_back(pq.top()-1);
+                pq.pop(); 
                 task++;
             }
             for(int x:store)
                 pq.push(x);
+            ans+=pq.empty()?task:n+1;
             
-            time+=pq.empty()?task:n+1;
         }
-        return time;
+        return ans;
     }
 };
