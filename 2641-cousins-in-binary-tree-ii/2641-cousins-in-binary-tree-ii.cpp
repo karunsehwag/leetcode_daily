1b5ebb81b87a89878f6178970c033625c5d8ed06
KARUN CHOUDHARY <72781081+karunsehwag@users.noreply.github.com>
2024-10-22 18:55:00 +0530

    Time: 8 ms (99.35%), Space: 314.7 MB (26.36%) - LeetHub

--- 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
@@ -1,54 +1,45 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    vector<int> vec;
-    void helper(TreeNode* root,int x)
-    {
-        if(root==NULL)
+    vector<int> levelSum;
+
+    void calculateLevelSum(TreeNode* node, int depth) {
+        if (node == nullptr) {
             return;
-        if(x>=vec.size())
-            vec.push_back(root->val);
-        else
-        {
-            vec[x]+=root->val;
         }
-        x++;
-        helper(root->left,x);
-        helper(root->right,x);
+
+        if (depth >= levelSum.size()) {
+            levelSum.push_back(node->val);
+        } else {
+            levelSum[depth] += node->val;
+        }
+
+        calculateLevelSum(node->left, depth + 1);
+        calculateLevelSum(node->right, depth + 1);
     }
-    void helper2(TreeNode* root,int x)
-    {
-        if(root==NULL)
+
+    void updateNodeValues(TreeNode* node, int depth) {
+        if (node == nullptr) {
             return;
-        if(root->left&&root->right)
-        {   int store=root->left->val+root->right->val;
-            root->right->val=vec[x]-store;
-            root->left->val=vec[x]-store;
         }
-         else if(root->left)
-             root->left->val=vec[x]-root->left->val;
-        else if(root->right)
-             root->right->val=vec[x]-root->right->val;
-        x++;
-        helper2(root->left,x);
-        helper2(root->right,x);  
+
+        if (node->left && node->right) {
+            int childrenSum = node->left->val + node->right->val;
+            node->left->val = levelSum[depth] - childrenSum;
+            node->right->val = levelSum[depth] - childrenSum;
+        } else if (node->left) {
+            node->left->val = levelSum[depth] - node->left->val;
+        } else if (node->right) {
+            node->right->val = levelSum[depth] - node->right->val;
+        }
+
+        updateNodeValues(node->left, depth + 1);
+        updateNodeValues(node->right, depth + 1);
     }
-    TreeNode* replaceValueInTree(TreeNode* root) 
-    {
-        TreeNode* head=root;
-        helper(root,0);
-        root->val=vec[0]-root->val;
-        helper2(root,1);
-        return root;    
+
+    TreeNode* replaceValueInTree(TreeNode* root) {
+        calculateLevelSum(root, 0);
+        root->val = levelSum[0] - root->val;
+        updateNodeValues(root, 1);
+        return root;
     }
 };
