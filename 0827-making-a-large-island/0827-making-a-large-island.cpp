94104a419c1bd1ece925ce43063eff37df32b279
KARUN CHOUDHARY <72781081+karunsehwag@users.noreply.github.com>
2024-10-31 13:57:53 +0530

    Time: 188 ms (75.00%), Space: 108.4 MB (90.01%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.cpp
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -1,110 +1,111 @@
+class Disjointset
+{
+    vector<int> size,parent;
+    public:
+    Disjointset(int n)
+    {
+        size.resize(n+1,1);
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++)
+        {
+            parent[i]=i;
+        }
+    }
+    
+    int findpar(int node)
+    {
+        if(node==parent[node])
+            return node;
+        return parent[node]=findpar(parent[node]);
+    }
+    
+void unionsize(int u ,int v)
+{
+    int u_u=findpar(u);
+    int v_v=findpar(v);
+    
+    if(u_u==v_v)
+    {
+        return;
+    }
+    else if(size[u_u]>size[v_v])
+    {
+        parent[v_v]=u_u;
+        size[u_u]+=size[v_v];
+    }
+    else 
+    {
+        parent[u_u]=v_v;
+        size[v_v]+=size[u_u];
+    }
+}
+    
+    int getsize(int n)
+    {
+        return size[findpar(n)];
+    }
+};
 class Solution {
 public:
-    class Disjointset
-    {
-        vector<int> parent,size;
-        public:
-        Disjointset(int n)
-        {
-            parent.resize(n+1);
-            size.resize(n+1,1);
-            for(int i=0;i<n;i++)
-            {
-                parent[i]=i;
-            }
-        }
-        int findupar(int node)
-        {
-            if(node==parent[node])
-                return node;
-            return parent[node]=findupar(parent[node]);
-        }
-        void unionsize(int u,int v)
-        {
-            int u_u=findupar(u);
-            int u_v=findupar(v);
-            if(u_v==u_u)
-                return;
-            if(size[u_u]>size[u_v])
-            {
-                parent[u_v]=u_u;
-                size[u_u]+=size[u_v];
-            }
-            else
-            {
-                parent[u_u]=u_v;
-                size[u_v]+=size[u_u];
-            }
-        }
-        
-        int getsize(int n)
-        {return size[n];}
-    };
     int largestIsland(vector<vector<int>>& grid) {
         
-        vector<vector<int>> adj;
-        vector<int> relative={-1,0,1,0,-1};
         int n=grid.size();
         Disjointset ds(n*n);
-        for(int row=0;row<grid.size();row++)
+        vector<int> vec={-1,0,1,0,-1};
+        
+        for(int i=0;i<n;i++)
         {
-            for(int col=0;col<grid[0].size();col++)
+            for(int j=0;j<n;j++)
+            {  if(grid[i][j]==1)
             {
-                if(grid[row][col]==1)
+                for(int k=0;k<vec.size()-1;k++)
                 {
-                    for(int i=0;i<4;i++)
+                    int newr=i+vec[k];
+                    int newc=j+vec[k+1];
+                    if(newr>=0&&newr<n&&newc>=0&&newc<n&&grid[newr][newc]==1)
                     {
-                        int newr=row+relative[i];
-                        int newc=col+relative[i+1];
-                        if(newr>=0&&newr<n&&newc>=0&&newc<n&&grid[newr][newc]==1)
-                        {
-                            int node=row*n+col;
-                            int adjnode=newr*n+newc;
-                            ds.unionsize(node,adjnode);
-                        }
+                        ds.unionsize(newr*n+newc,i*n+j);
                     }
                 }
             }
+            }
         }
-        
-        
-        int mx=0;
-         for(int row=0;row<grid.size();row++)
+        int ans=0;
+        for(int i=0;i<n;i++)
         {
-            for(int col=0;col<grid[0].size();col++)
-            {   if(grid[row][col]==1)
-                   continue;
-             
-                 set<int> component;
-                    for(int i=0;i<4;i++)
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]==1)
+                    continue;
+                set<int> component;
+                 for(int k=0;k<vec.size()-1;k++)
+                {
+                    int newr=i+vec[k];
+                    int newc=j+vec[k+1];
+                    if(newr>=0&&newr<n&&newc>=0&&newc<n&&grid[newr][newc]==1)
                     {
-                        int newr=row+relative[i];
-                        int newc=col+relative[i+1];
-                        if(newr>=0&&newr<n&&newc>=0&&newc<n&&grid[newr][newc]==1)
-                        {
-                            component.insert(ds.findupar(newr*n+newc));
-                        }
+                        component.insert(ds.findpar(newr*n+newc));
                     }
-             int sizetotal=0;
-             for(auto it:component)
-             {
-                 sizetotal+=ds.getsize(it);
-             }
-             mx=max(sizetotal+1,mx);
-             
+                }
+                int sizetotal=0;
+                for(int it:component)
+                {
+                    sizetotal+=ds.getsize(ds.findpar(it));
+                }
+                
+                ans=max(ans,sizetotal+1);
+                
                 
             }
         }
         
-        for(int i=0;i<n*n;i++)
+         for(int i=0;i<n*n;i++)
         {
-            mx=max(mx,ds.getsize(ds.findupar(i)));
+            ans=max(ans,ds.getsize(ds.findpar(i)));
         }  
         
-        return mx;
         
-    
-        
+        return ans;
         
     }
 };
