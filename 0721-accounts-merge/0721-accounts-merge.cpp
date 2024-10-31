db3b05411403ccd4ad6ff045b002879c34e6b1d6
KARUN CHOUDHARY <72781081+karunsehwag@users.noreply.github.com>
2024-10-31 17:13:49 +0530

    Time: 27 ms (92.28%), Space: 37.9 MB (75.73%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,45 +1,54 @@
+class Disjointset
+{
+    vector<int> parent,size;
+    public:
+    Disjointset(int n)
+    {
+        parent.resize(n+1);
+        size.resize(n+1,1);
+        for(int i=0;i<n;i++)
+        {
+            parent[i]=i;
+        }
+    }
+    
+    int findpar(int node)
+    {
+        if(node==parent[node])
+            return node;
+        return parent[node]=findpar(parent[node]);
+    }
+    void unionsize(int u,int v)
+    {
+        int u_u=findpar(u);
+        int v_v=findpar(v);
+        
+        if(u_u==v_v)
+            return;
+        if(size[u_u]>size[v_v])
+        {
+            parent[v_v]=u_u;
+            size[u_u]+=size[v_v];
+        }else
+        {
+            parent[u_u]=v_v;
+            size[v_v]+=size[u_u];
+        }
+    }
+    
+    int getsize(int node)
+    {
+        return size[findpar(node)];
+    }
+};
 class Solution {
 public:
-    class Disjointset
-    {
-        vector<int> parent,size;
-        public:
-        Disjointset(int n)
-        {
-            parent.resize(n+1);
-            size.resize(n+1,1);
-            for(int i=0;i<n;i++)
-            {parent[i]=i;}
-        }
-        int findupar(int node)
-        {
-            if(parent[node]==node)
-                return node;
-            return parent[node]=findupar(parent[node]);
-        }
-        void unionfind(int u,int v)
-        {
-            int u_u=findupar(u);
-            int u_v=findupar(v);
-            if(u_u==u_v)
-                return;
-            if(size[u_u]>size[u_v])
-            {
-                parent[u_v]=u_u;
-                size[u_u]+=size[u_v];
-            }
-            else
-            {
-                parent[u_u]=u_v;
-                size[u_v]+=size[u_u]; 
-            }
-        }
-    };
-    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) 
-    {
-        int n=accounts.size();
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        
+        vector<vector<string>> ans;
         unordered_map<string,int> mp;
-        Disjointset Ds(n);
+        int n=accounts.size();
+        Disjointset ds(n);
         for(int i=0;i<accounts.size();i++)
         {
             for(int j=1;j<accounts[i].size();j++)
@@ -50,36 +59,32 @@ public:
                 }
                 else
                 {
-                    Ds.unionfind(i,mp[accounts[i][j]]);
+                    ds.unionsize(i,mp[accounts[i][j]]);
                 }
-                
             }
         }
-            vector<string> mergedmail[n];
-            
-            for(auto it:mp)
+        vector<string> vec[accounts.size()];
+        for(auto it:mp)
+        {
+            string node=it.first;
+            int val=ds.findpar(it.second);
+            vec[val].push_back(node);
+        }
+        for(int i=0;i<accounts.size();i++)
+        {
+            if(vec[i].size()==0)
+                continue;
+            sort(vec[i].begin(),vec[i].end());
+            vector<string> temp;
+            temp.push_back(accounts[i][0]);
+            for(string str:vec[i])
             {
-                string node=it.first;
-                int val=Ds.findupar(it.second);
-                mergedmail[val].push_back(node);
-                
+                temp.push_back(str);
             }
-            vector<vector<string>> ans;
-            for(int i=0;i<n;i++)
-            {
-                if(mergedmail[i].size()==0) continue;
-                sort(mergedmail[i].begin(),mergedmail[i].end());
-                vector<string> temp;
-                temp.push_back(accounts[i][0]);
-                for(auto it:mergedmail[i])
-                {
-                    temp.push_back(it);
-                }
-                ans.push_back(temp);
-            }
-            
-            sort(ans.begin(),ans.end());        
-         return ans;
+            ans.push_back(temp);
+        }
+        
+        return ans;
         
     }
 };
