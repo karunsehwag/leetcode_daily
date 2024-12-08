219035dd90a65859f5c145f1a1e748c78535350a
KARUN CHOUDHARY <72781081+karunsehwag@users.noreply.github.com>
2024-12-08 21:29:44 +0530

    Time: 10 ms (89.16%), Space: 34.8 MB (69.58%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,65 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        
-        vector<vector<int>> vec(mat.size(),vector<int>(mat[0].size(),INT_MAX));
-        
-        queue<pair<int,int>> q;
-        for(int i=0;i<mat.size();i++)
-        {
-            for(int j=0;j<mat[0].size();j++)
-            {
-                 if(mat[i][j]==0)
-                 {q.push({i,j});
-                    vec[i][j]=0;};
+        // Initialize a matrix to store the distances, with all elements set to INT_MAX
+        int rows = mat.size();
+        int cols = mat[0].size();
+        vector<vector<int>> distances(rows, vector<int>(cols, INT_MAX));
+
+        queue<pair<int, int>> q;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (mat[i][j] == 0) {
+                    q.push({i, j}); 
+                    distances[i][j] = 0;
+                }
             }
         }
-        while(!q.empty())
-        {
-            auto node=q.front();
+
+
+        vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+
+        while (!q.empty()) {
+            auto [x, y] = q.front();
             q.pop();
-            int x=node.first;
-            int y=node.second;
-            if(x-1>=0)
-            {
-                
-                if(vec[x-1][y]==INT_MAX)
-                {
-                    q.push({x-1,y});
-                    vec[x-1][y]=vec[x][y]+1;
+
+            for (auto [dx, dy] : directions) {
+                int newX = x + dx;
+                int newY = y + dy;
+
+                if (newX >= 0 && newX < rows && newY >= 0 && newY < cols) {
+                    if (distances[newX][newY] == INT_MAX) { 
+                        distances[newX][newY] = distances[x][y] + 1; 
+                        q.push({newX, newY});
+                    }
                 }
             }
-            if(y-1>=0)
-            {
-               
-                if(vec[x][y-1]==INT_MAX)
-                {
-                    q.push({x,y-1});
-                    vec[x][y-1]=vec[x][y]+1;
-                }
-            }
-            if(mat.size()>x+1)
-            {
-                if(vec[x+1][y]==INT_MAX)
-                {
-                    q.push({x+1,y});
-                    vec[x+1][y]=vec[x][y]+1;
-                }
-            }
-            if(mat[0].size()>y+1)
-            {
-              
-                if(vec[x][y+1]==INT_MAX)
-                {
-                    q.push({x,y+1});
-                    vec[x][y+1]=vec[x][y]+1;
-                }
-            }
-            
-            
-            
         }
-        return vec;
-        
+
+        return distances;
     }
 };
